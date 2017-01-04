17b503724c30e398376403efd54e1b91d2496b6a
JacketWhite <feherkeee@gmail.com>
2017-01-04 09:59:40 +0100

    controller skeleton

--- src/main/java/email_sender_microservice/controller/EmailController.java
+++ src/main/java/email_sender_microservice/controller/EmailController.java
@@ -4,9 +4,13 @@ package email_sender_microservice.controller;
 import spark.Request;
 import spark.Response;
 
-public class EmailAPIServiceController {
+public class EmailController {
 
     public String status(Request request, Response response) {
         return "ok";
+    }
+
+    public boolean createEmail(Request request, Response response) {
+        return true;
     }
 }
