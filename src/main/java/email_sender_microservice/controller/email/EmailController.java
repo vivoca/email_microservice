a73d1d3d89a095d7ba40bd43e298f95bee02ba09
Mónika Lombos <lombos.monika@gmail.com>
2017-01-10 19:42:25 +0100

    Some part of outsourced

--- src/main/java/email_sender_microservice/controller/email/EmailController.java
+++ src/main/java/email_sender_microservice/controller/email/EmailController.java
@@ -2,28 +2,28 @@ package email_sender_microservice.controller.email;
 
 
 import com.j256.ormlite.dao.Dao;
-import email_sender_microservice.model.Client;
+import com.j256.ormlite.dao.DaoManager;
 import email_sender_microservice.model.Email;
 import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
 
+import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
-public class EmailController {
-
+public class EmailController extends AbstractConnection{
 
     public String status(Request request, Response response) {
         return "ok";
     }
 
-    public boolean createEmail(Request request, Response response, Dao<Email, String> emailDao) {
+    public boolean createEmail(Request request, Response response) throws SQLException {
+        Dao<Email, String> emailDao = DaoManager.createDao(getConnectionSource(), Email.class);
+
         JSONObject emailDetail = new JSONObject(request.body());
         ArrayList<String> keyList = new ArrayList<>(Arrays.asList("to", "from", "subject", "message"));
         for (String key : keyList) if (emailDetail.isNull(key)) return false;
@@ -43,7 +43,7 @@ public class EmailController {
         return true;
     }
 
-    public boolean register(Request request, Response response, Dao<Client, String> clientDao) {
+    public boolean register(Request request, Response response) {
         return true;
     }
 
@@ -58,14 +58,55 @@ public class EmailController {
         return result;
     }
 
+    public static void send(String clientEmail, String userEmail, String subject, String messagetext) throws SQLException {
+        Dao<Email, String> emailDao = DaoManager.createDao(getConnectionSource(), Email.class);
+        try {
+            System.out.println("TLSEmail Start");
+            Properties props = new Properties();
+            props.put("mail.smtp.host", SMTP_HOST);
+            props.put("mail.smtp.port", SMTP_PORT);
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+
+            //create Authenticator object to pass in Session.getInstance argument
+            Authenticator auth = new Authenticator() {
+                //override the getPasswordAuthentication method
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(SENDER_EMAIL, SENDER_PASSWORD);
+                }
+            };
+            Session session = Session.getInstance(props, auth);
+
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(SENDER_EMAIL));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(userEmail));
+            message.setReplyTo(new Address[]{new InternetAddress(clientEmail)});
+
+            System.out.println("Mail Check 2");
+
+            message.setSubject(subject);
+            message.setContent(messagetext, "text/html");
+
+            System.out.println("Mail Check 3");
+
+            Transport.send(message);
+            System.out.println("Mail Sent");
+        } catch (Exception ex) {
+            System.out.println("Mail fail");
+            System.out.println(ex);
+        }
+    }
 
     public void scheduleEmails(final String sender, final String receiver, final String subject, final String message){
-        final EmailSender email1 = new EmailSender();
         Timer t = new Timer();
         t.scheduleAtFixedRate(
                 new TimerTask(){
                     public void run() {
-                        email1.send(sender, receiver, subject, message);
+                        try {
+                            send(sender, receiver, subject, message);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 0,
--- src/main/java/email_sender_microservice/server/EmailReceiverServer.java
+++ src/main/java/email_sender_microservice/server/EmailReceiverServer.java
@@ -3,50 +3,38 @@ package email_sender_microservice.server;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.logger.Logger;
 import com.j256.ormlite.logger.LoggerFactory;
-import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
-import email_sender_microservice.controller.ConnectionHandling;
+import email_sender_microservice.controller.email.AbstractConnection;
 import email_sender_microservice.controller.email.EmailController;
 import email_sender_microservice.model.Client;
 import email_sender_microservice.model.Email;
 
 import java.net.URISyntaxException;
 import java.sql.SQLException;
-import java.util.HashMap;
 
 import static spark.Spark.*;
 
 public class EmailReceiverServer {
     private static final Logger logger = LoggerFactory.getLogger(EmailReceiverServer.class);
-    private static ConnectionHandling configReader = new ConnectionHandling();
-    protected static HashMap DBprops = configReader.getPropValues();
-
-    private static final String DATABASE = "jdbc:postgresql://" + DBprops.get("url") + "/" + DBprops.get("database");
-    private static final String DB_USER = String.valueOf(DBprops.get("user"));
-    private static final String DB_PASSWORD = String.valueOf(DBprops.get("password"));
-
 
     public static void main(String[] args) throws SQLException {
         logger.debug("Starting " + EmailReceiverServer.class.getName() + "...");
-        ConnectionSource connectionSource = new JdbcConnectionSource(DATABASE, DB_USER, DB_PASSWORD);
 
         setup(args);
-//        port(60000);
-        TableUtils.createTableIfNotExists(connectionSource, Email.class);
-        TableUtils.createTableIfNotExists(connectionSource, Client.class);
+        TableUtils.createTableIfNotExists(AbstractConnection.getConnectionSource(), Email.class);
+        TableUtils.createTableIfNotExists(AbstractConnection.getConnectionSource(), Client.class);
 
-        Dao<Email, String> emailDao = DaoManager.createDao(connectionSource, Email.class);
-        Dao<Client, String> clientDao = DaoManager.createDao(connectionSource, Client.class);
+        Dao<Email, String> emailDao = DaoManager.createDao(AbstractConnection.getConnectionSource(), Email.class);
+        Dao<Client, String> clientDao = DaoManager.createDao(AbstractConnection.getConnectionSource(), Client.class);
 
         EmailController controller = new EmailController();
 
         // --- MAPPINGS ---
         get("/api/status", controller::status);
-        post("/api/create", (request, response) -> controller.createEmail(request, response, emailDao));
-        post("/api/register", ((request, response) -> controller.register(request, response, clientDao)));
+        post("/api/create", (request, response) -> controller.createEmail(request, response));
+        post("/api/register", ((request, response) -> controller.register(request, response)));
 
         // --- EXCEPTION HANDLING ---
         exception(URISyntaxException.class, (exception, request, response) -> {
