0a0a441303d77f71e72ac2ed55a7cb9cd2872548
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-01-11 18:02:00 +0100

    exception handling

--- src/main/java/email_sender_microservice/Main.java
+++ src/main/java/email_sender_microservice/Main.java
@@ -1,10 +1,23 @@
 package email_sender_microservice;
 
+import email_sender_microservice.server.EmailReceiverServer;
+import email_sender_microservice.server.EmailSenderServer;
+
+import java.sql.SQLException;
+
 
 public class Main {
+    public static void main(String[] args) {
 
-    // run EmailReceiveServer
+        // run EmailReceiverServer
+        try {
+            EmailReceiverServer.main();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // run EmailSenderServer
+        EmailSenderServer.main();
 
 
-    // run EmailSenderServer
+    }
 }
