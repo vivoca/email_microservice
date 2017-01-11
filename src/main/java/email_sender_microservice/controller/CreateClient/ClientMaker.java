d8d5ce59bb787453ae0b72bf55de7ec1b41d4d7f
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-01-11 17:57:54 +0100

    almost done

--- src/main/java/email_sender_microservice/controller/CreateClient/ClientMaker.java
+++ src/main/java/email_sender_microservice/controller/CreateClient/ClientMaker.java
@@ -4,7 +4,9 @@ package email_sender_microservice.controller.CreateClient;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import email_sender_microservice.controller.email.AbstractConnection;
+import email_sender_microservice.controller.email.EmailController;
 import email_sender_microservice.model.Client;
+import email_sender_microservice.model.Email;
 
 import java.sql.SQLException;
 
@@ -13,9 +15,10 @@ public class ClientMaker extends AbstractConnection{
     public static void main(String[] args) {
         try {
             Dao<Client, String> clientDao =  DaoManager.createDao(getConnectionSource(), Client.class);
-            Client newClient = new Client("name", "password", "headerURL", "FooterURL", "textRGBColor");
+            Client newClient = new Client("PindurPandurok", "password", "http://cartoonbros.com/powerpuff-girls/powerpuff-girls-6/", "http://cartoonbros.com/powerpuff-girls/powerpuff-girls-6/", "blue");
             clientDao.create(newClient);
             logger.info("Client Successfully saved.");
+            //TODO: Send email to the new Client about his/her APIKey
         } catch (SQLException e) {
             logger.error("Client creation failed. Check constructor parameters.");
             e.printStackTrace();
