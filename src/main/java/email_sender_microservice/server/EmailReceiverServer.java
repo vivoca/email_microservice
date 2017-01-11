5e4aa2560d7013630a6035b205761f100ff95ef7
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-01-11 18:00:10 +0100

    port can be modified in the connection.properties, default is 60000

--- src/main/java/email_sender_microservice/server/EmailReceiverServer.java
+++ src/main/java/email_sender_microservice/server/EmailReceiverServer.java
@@ -1,11 +1,10 @@
 package email_sender_microservice.server;
 
 
-import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.logger.Logger;
 import com.j256.ormlite.logger.LoggerFactory;
 import com.j256.ormlite.table.TableUtils;
+import email_sender_microservice.controller.ConnectionHandling;
 import email_sender_microservice.controller.email.AbstractConnection;
 import email_sender_microservice.controller.email.EmailController;
 import email_sender_microservice.model.Client;
@@ -13,28 +12,31 @@ import email_sender_microservice.model.Email;
 
 import java.net.URISyntaxException;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 import static spark.Spark.*;
 
 public class EmailReceiverServer {
     private static final Logger logger = LoggerFactory.getLogger(EmailReceiverServer.class);
+    private static ConnectionHandling configReader = new ConnectionHandling();
+    private static HashMap <String, String> properties = configReader.getPropValues();
+    private static final int port = Integer.valueOf(properties.getOrDefault("port", "60000"));
 
-    public static void main(String[] args) throws SQLException {
+    public static void main() throws SQLException {
+
         logger.debug("Starting " + EmailReceiverServer.class.getName() + "...");
 
-        setup(args);
+        setup(port);
         TableUtils.createTableIfNotExists(AbstractConnection.getConnectionSource(), Email.class);
         TableUtils.createTableIfNotExists(AbstractConnection.getConnectionSource(), Client.class);
 
-        Dao<Email, String> emailDao = DaoManager.createDao(AbstractConnection.getConnectionSource(), Email.class);
-        Dao<Client, String> clientDao = DaoManager.createDao(AbstractConnection.getConnectionSource(), Client.class);
 
         EmailController controller = new EmailController();
 
         // --- MAPPINGS ---
         get("/api/status", controller::status);
-        post("/api/create", (request, response) -> controller.createEmail(request, response));
-        post("/api/register", ((request, response) -> controller.register(request, response)));
+        post("/api/create", controller::createEmail);
+        post("/api/register", (controller::register));
 
         // --- EXCEPTION HANDLING ---
         exception(URISyntaxException.class, (exception, request, response) -> {
@@ -54,16 +56,11 @@ public class EmailReceiverServer {
 //     * Setting up port
 //     * @param args - app args
 //     */
-    private static void setup(String[] args){
-        if(args == null || args.length == 0){
-            logger.error("Port must be given as the first argument.");
-            System.exit(-1);
-        }
-
+    private static void setup(int port){
         try {
-            port(Integer.parseInt(args[0]));
+            port(port);
         } catch (NumberFormatException e){
-            logger.error("Invalid port given '{}', it should be number.", args[0]);
+            logger.error("Invalid port given '{}', it should be number.", port);
             System.exit(-1);
         }
     }
