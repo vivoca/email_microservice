481fd9b86fe9626532ed4aa04ab65dce08d4140f
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-01-11 17:58:19 +0100

    email queries are ready, untested

--- src/main/java/email_sender_microservice/controller/email/EmailController.java
+++ src/main/java/email_sender_microservice/controller/email/EmailController.java
@@ -3,7 +3,11 @@ package email_sender_microservice.controller.email;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
+import email_sender_microservice.model.Client;
 import email_sender_microservice.model.Email;
+import email_sender_microservice.model.enums.EmailStatus;
 import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
@@ -23,16 +27,23 @@ public class EmailController extends AbstractConnection{
 
     public boolean createEmail(Request request, Response response) throws SQLException {
         Dao<Email, String> emailDao = DaoManager.createDao(getConnectionSource(), Email.class);
+        Dao<Client, String> clientDao = DaoManager.createDao(getConnectionSource(), Client.class);
+
 
         JSONObject emailDetail = new JSONObject(request.body());
         ArrayList<String> keyList = new ArrayList<>(Arrays.asList("to", "from", "subject", "message"));
         for (String key : keyList) if (emailDetail.isNull(key)) return false;
 
+        // This is how you write a query in ORMLite 'SELECT * FROM client WHERE APIKey == APIKey(from request body)'
+        PreparedQuery<Client> prepClientQuery = clientDao.queryBuilder().where().eq("APIKey", emailDetail.getString("APIKey")).prepare();
+        Client client = clientDao.queryForFirst(prepClientQuery);
+
         if (isValidEmailAddress(emailDetail.getString("to")) && isValidEmailAddress(emailDetail.getString("from"))) {
             Email email = new Email(emailDetail.getString("to"),
                                     emailDetail.getString("from"),
                                     emailDetail.getString("subject"),
-                                    emailDetail.getString("message"));
+                                    emailDetail.getString("message"),
+                                    client);
 
             try {
                 emailDao.create(email);
@@ -58,8 +69,14 @@ public class EmailController extends AbstractConnection{
         return result;
     }
 
-    public static void send(String clientEmail, String userEmail, String subject, String messagetext) throws SQLException {
-        Dao<Email, String> emailDao = DaoManager.createDao(getConnectionSource(), Email.class);
+    public static boolean send(Email email)  {
+
+        String messagetext = "<body style='text-align: justify; color: " +
+                email.getClient().getTextColor() + "'><img src=" +
+                email.getClient().getHeader() + " style='width: 100%'>" +
+                email.getMessage() + "<img src=" +
+                email.getClient().getFooter() +
+                "></body>";
         try {
             System.out.println("TLSEmail Start");
             Properties props = new Properties();
@@ -78,35 +95,42 @@ public class EmailController extends AbstractConnection{
             Session session = Session.getInstance(props, auth);
 
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(SENDER_EMAIL));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(userEmail));
-            message.setReplyTo(new Address[]{new InternetAddress(clientEmail)});
+            message.setFrom(new InternetAddress(email.getFrom()));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(email.getTo()));
+            message.setReplyTo(new Address[]{new InternetAddress(email.getFrom())});
 
             System.out.println("Mail Check 2");
 
-            message.setSubject(subject);
+            message.setSubject(email.getSubject());
             message.setContent(messagetext, "text/html");
 
             System.out.println("Mail Check 3");
 
             Transport.send(message);
             System.out.println("Mail Sent");
+            return true;
         } catch (Exception ex) {
             System.out.println("Mail fail");
-            System.out.println(ex);
+            return false;
         }
     }
 
-    public void scheduleEmails(final String sender, final String receiver, final String subject, final String message){
+    public void scheduleEmails() throws SQLException {
+        Dao<Email, String> emailDao = DaoManager.createDao(getConnectionSource(), Email.class);
         Timer t = new Timer();
         t.scheduleAtFixedRate(
                 new TimerTask(){
                     public void run() {
                         try {
-                            send(sender, receiver, subject, message);
-                        } catch (SQLException e) {
-                            e.printStackTrace();
-                        }
+                            List<Email> emails = emailDao.queryBuilder().where().eq("status", EmailStatus.NEW).query();
+                            for (Email email : emails) {
+                                   if(send(email)){
+                                       email.setStatus(EmailStatus.SENT);
+                                       emailDao.update(email);
+                                   }
+
+                            }
+                        }catch (SQLException e) {e.printStackTrace();}
                     }
                 },
                 0,
