fb353ffff6a0204519ca1036b5ef56a1eebd795f
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-01-13 13:32:17 +0100

    password is removed from constructor

--- src/main/java/email_sender_microservice/controller/CreateClient/ClientMaker.java
+++ src/main/java/email_sender_microservice/controller/CreateClient/ClientMaker.java
@@ -15,10 +15,9 @@ public class ClientMaker extends AbstractConnection{
     public static void main(String[] args) {
         try {
             Dao<Client, String> clientDao =  DaoManager.createDao(getConnectionSource(), Client.class);
-            Client newClient = new Client("PindurPandurok", "password", "http://cartoonbros.com/powerpuff-girls/powerpuff-girls-6/", "http://cartoonbros.com/powerpuff-girls/powerpuff-girls-6/", "blue");
+            Client newClient = new Client("PindurPandurok", "http://cartoonbros.com/powerpuff-girls/powerpuff-girls-6/", "http://cartoonbros.com/powerpuff-girls/powerpuff-girls-6/", "blue");
             clientDao.create(newClient);
             logger.info("Client Successfully saved.");
-            //TODO: Send email to the new Client about his/her APIKey
         } catch (SQLException e) {
             logger.error("Client creation failed. Check constructor parameters.");
             e.printStackTrace();
